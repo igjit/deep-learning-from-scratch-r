292c3d5e410f5435289d1521e22217fce9b3145a
igjit <igjit1@gmail.com>
2022-10-23 23:59:38 +0900

    TwoLayerNet

--- common/functions.R
+++ common/functions.R
@@ -2,3 +2,7 @@ softmax <- function(a) {
   exp_a <- exp(a - max(a))
   exp_a / sum(exp_a)
 }
+
+sigmoid <- function(x) 1 / (1 + exp(-x))
+
+rep_row <- function(x, n) matrix(rep(x, n), n, byrow = TRUE)
